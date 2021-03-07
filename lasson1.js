9df0f562667dbb388efec9c2702422ada2f30007
Hennadiy <45887822+geen92@users.noreply.github.com>
2021-03-07 11:02:38 +0200

    Create lasson1.js

---
+++ lasson1.js
@@ -0,0 +1,36 @@
+// Ponomarov. Lesson 1
+
+// ex.1
+
+var cat = "cat";
+var dog = "dog";
+// [cat, dog] = [dog, cat];
+x = cat
+cat = dog
+dog = x
+console.log(cat)
+console.log(dog)
+
+// ex.2
+
+let company
+let position
+company = "Trinity";
+position = "QA";
+console.log(company)
+console.log(position)
+
+// ex.3
+
+const isProductCompany = true
+let productName
+console.log(typeof(isProductCompany))
+console.log(typeof(company))
+console.log(typeof(position))
+
+// ex.4
+
+productName = "Sweet.TV"
+console.log(productName)
+console.log(typeof(productName))
+
