b58015ade7437184faf936dd140fd1e2afb4f4c9
Hennadiy <45887822+geen92@users.noreply.github.com>
2021-03-14 14:19:31 +0200

    Add files via upload

---
+++ Ponomarov lesson 2 Work on bugs .js
@@ -0,0 +1,25 @@
+// Ponomarov. Lesson 2
+// Работа над ошибками
+
+// ex.1
+
+let a = 4
+let b = 4
+
+let result = (a + b < 10) ? true : false; if (result == true){
+    console.log("Few")
+} else {
+console.log("Many")
+}
+
+// ex.3
+
+ //Запускать код в браузере
+/*   
+let name = prompt("Как тебя зовут?");
+alert(`Привет ${name}, рад тебя видеть`);
+let year = prompt("В какой году ты родился?");
+let currentYears = ( new Date().getFullYear() );
+alert (`Твой возраст = ${(currentYears - year)}`);
+
+*/
