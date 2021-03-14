0dd83cfaf2a34696d2868192230c0e934e5c6126
Hennadiy <45887822+geen92@users.noreply.github.com>
2021-03-14 11:45:41 +0200

    Add files via upload

---
+++ Ponomarov lesson 2.js
@@ -0,0 +1,56 @@
+// Ponomarov. Lesson 2
+
+// ex.1
+
+let a = 1
+let b = 7
+let result = (a + b < 10); if (result === true) {
+    console.log("Few"); /* Мельше */
+} else if (result === false) { console.log("Many"); /* Больше */}
+
+// ex.2
+
+let age = 29;
+if (age >= 18 && age <= 90) {
+    console.log('Вам от 18 до 90 лет!');
+} else if (age < 18) {
+    console.log('Вам меньньше 18!');
+} else {
+    console.log('Вам больше чем 90 лет!');
+}
+
+// ex.3
+
+            //Запускать код в браузере
+/*   
+let name = prompt;
+alert("Привет " + name("Как тебя зовут?") + " , рад тебя видеть!")
+let year = prompt("В какой году ты родился?");
+alert ("Твой возраст = " + (2021 - year));
+*/
+
+// ex.4
+
+let number = 99
+if (number < 60) { console.log("F"); 
+} else if (number < 70) { console.log("D")
+} else if (number < 80) { console.log("C")
+} else if (number < 90) { console.log("B")
+} else if (number < 100) { console.log("A")
+}
+
+            //Запускать код в браузере
+/*   
+let number = prompt("Введите число от 0 до 99"); if (number < 60) {
+    alert("F"); 
+} else if (number < 70) { alert("D")
+} else if (number < 80) { alert("C")
+} else if (number < 90) { alert("B")
+} else if (number < 100) { alert("A")
+}
+*/
+
+
+
+
+ 
