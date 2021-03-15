fe66ae7c35ebe80dbca2d90938b7f3fba34c2048
Hennadiy <45887822+geen92@users.noreply.github.com>
2021-03-15 09:32:37 +0200

    Update Ponomarov lesson 2 Work on bugs .js

--- Ponomarov lesson 2 Work on bugs .js
+++ Ponomarov lesson 2 Work on bugs .js
@@ -12,6 +12,13 @@ let result = (a + b < 10) ? true : false; if (result == true){
 console.log("Many")
 }
 
+// ex.1 Работа над ошибками №2
+
+let a = 4
+let b = 5
+
+let result = (a + b < 10) ? console.log("Few") : console.log("Many"); 
+
 // ex.3
 
  //Запускать код в браузере
@@ -22,4 +29,14 @@ let year = prompt("В какой году ты родился?");
 let currentYears = ( new Date().getFullYear() );
 alert (`Твой возраст = ${(currentYears - year)}`);
 
+*/
+
+// ex.3 Работа над ошибками
+
+/*
+let name = prompt("Как тебя зовут?");
+alert(`Привет ${name}, рад тебя видеть`);
+let year = prompt("В какой году ты родился?");
+let currentYears = new Date().getFullYear();
+alert (`Твой возраст = ${(currentYears - year)}`);
 */
